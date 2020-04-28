8ef84a2b8719946dba3249392c4bc1a420c80ebb
andy-caress <54376326+andy-caress@users.noreply.github.com>
2020-04-28 19:53:34 -0400

    2

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
-$(window).scroll(function() {
-var theta = $(window).scrollTop() / 10 % Math.PI;
-$('#leftgear').css({ transform: 'rotate(' + theta + 'rad)' });
-$('#rightgear').css({ transform: 'rotate(-' + theta + 'rad)' });
-});
+$(function() {
+  var rotation = 0,
+    scrollLoc = $(document).scrollTop();
+  $(window).scroll(function() {
+    var newLoc = $(document).scrollTop();
+    var diff = scrollLoc - newLoc;
+    rotation += diff, scrollLoc = newLoc;
+    var rotationStr = "rotate(" + rotation + "deg)";
+    $("#left").css({
+      '-moz-transform': 'rotate(' + offset + 'deg)',
+      '-webkit-transform': 'rotate(' + offset + 'deg)',
+      '-o-transform': 'rotate(' + offset + 'deg)',
+      '-ms-transform': 'rotate(' + offset + 'deg)',
+      'transform': 'rotate(' + offset + 'deg)',
+    });
+  });
+})
